42c1aca56cd4985b9016da2f6e44cd3a7aa29663
chensiyou <905630790@qq.com.cn>
2019-07-21 18:47:29 +0800

    feat:"初始化项目"

---
+++ src/main/java/com/csy/hrserver/Activittese3Application.java
@@ -0,0 +1,13 @@
+package com.csy.hrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activittese3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Activittese3Application.class, args);
+    }
+
+}
